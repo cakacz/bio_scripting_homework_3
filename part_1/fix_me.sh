d75ae24e8282dd42c48f267c3207202d25551c61
Chloe Kaczvinsky <cak0052@tigermail.auburn.edu>
2019-02-11 09:39:56 -0600

    Removed unnecessary indents to improve readability

--- part_1/fix_me.sh
+++ part_1/fix_me.sh
@@ -5,25 +5,25 @@ read -p 'Enter a : ' a
 read -p 'Enter b (not 0) : ' b
 
 add=$((a+b))
-        echo "Addition of a and b are $add"
+echo "Addition of a and b are $add"
 
 sub=$((a-b))
-        echo "Subtraction of a and b are $sub"
+echo "Subtraction of a and b are $sub"
 
 mul=$((a*b))
-        echo "Multiplication of a and b are $mul"
+echo "Multiplication of a and b are $mul"
 
 div=$((a/b))
-        echo "division of a and b are $div"
+echo "division of a and b are $div"
 
 mod=$((a%b))
-          echo "Modulus of a and b are $mod"
+echo "Modulus of a and b are $mod"
 
-      ((++a))
-          echo "Increment operator when applied on "a" results into a = $a"
+((++a))
+echo "Increment operator when applied on "a" results into a = $a"
 
-      ((--b))
-          echo "Decrement operator when applied on "b" results into b = $b"
+((--b))
+echo "Decrement operator when applied on "b" results into b = $b"
 
 (
 (--c)
