26ce4300f9f3b2a41934fbafb245081046f2c1b4
Chloe Kaczvinsky <cak0052@tigermail.auburn.edu>
2019-02-12 14:42:15 -0600

    Commented out echo statements and added three lines to append variable outputs to sys_info_log.txt file

--- part_2/sys_info_Kaczvinsky.sh
+++ part_2/sys_info_Kaczvinsky.sh
@@ -5,13 +5,18 @@
 
 #define variable to return data and time
 datetime=`date | awk '{print $2, $3, $4}'`
-echo "$datetime"
+#echo "$datetime"
 
 #define variable to return logged in users
 logusers=`who | cut -d' ' -f1`
-echo "$logusers"
+#echo "$logusers"
 
 #define variable for system uptime
 systime=`uptime`
-echo "$systime"
+#echo "$systime"
+
+#redirect outputs to a file
+datetime>>sys_info_log.txt
+logusers>>sys_info_log.txt
+systime>>>sys_info_log.txt
 
