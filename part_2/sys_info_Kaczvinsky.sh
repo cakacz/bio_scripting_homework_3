29f283cee6f0eb84f61a86625ad7087ee9742db1
Chloe Kaczvinsky <cak0052@tigermail.auburn.edu>
2019-02-12 12:12:53 -0600

    Added pseudo-code outline of eventual script to file

--- part_2/sys_info_Kaczvinsky.sh
+++ part_2/sys_info_Kaczvinsky.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+#script will display time and data, all logged-in users, and the system uptime 
+#will then write  these variables to log file in readable format
+
+#define variable to return data and time
+
+#define variable to return logged in users
+
+#define variable for system uptime
+
+
