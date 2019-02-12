efb7435adb34fad527c975b33283300e4bf58d53
Chloe Kaczvinsky <cak0052@tigermail.auburn.edu>
2019-02-12 14:48:24 -0600

    Uncommented echo statements and added lines to add output of echo to sys_info_log.txt to the echo lines, removed last three lines attempting to append sys_info_log.txt file

--- part_2/sys_info_Kaczvinsky.sh
+++ part_2/sys_info_Kaczvinsky.sh
@@ -5,18 +5,12 @@
 
 #define variable to return data and time
 datetime=`date | awk '{print $2, $3, $4}'`
-#echo "$datetime"
+echo "$datetime" >> sys_info_log.txt
 
 #define variable to return logged in users
 logusers=`who | cut -d' ' -f1`
-#echo "$logusers"
+echo "$logusers" >> sys_info_log.txt
 
 #define variable for system uptime
 systime=`uptime`
-#echo "$systime"
-
-#redirect outputs to a file
-datetime>>sys_info_log.txt
-logusers>>sys_info_log.txt
-systime>>>sys_info_log.txt
-
+echo "$systime" >> sys_info_log.txt
