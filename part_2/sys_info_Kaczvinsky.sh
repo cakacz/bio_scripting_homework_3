882a3526998bd133539a7e8112d1a1523dd39529
Chloe Kaczvinsky <cak0052@tigermail.auburn.edu>
2019-02-12 12:23:15 -0600

    Added basic pipeline to get date.time and log.users variables

--- part_2/sys_info_Kaczvinsky.sh
+++ part_2/sys_info_Kaczvinsky.sh
@@ -4,8 +4,11 @@
 #will then write  these variables to log file in readable format
 
 #define variable to return data and time
+time.date=`date | awk '{print $4}'`
+
 
 #define variable to return logged in users
+log.users=`who | cut -d'' -f1`
 
 #define variable for system uptime
 
